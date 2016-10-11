6907532bbe230bbe2020ca0a9f22c7e75f710745
SurfRockRick <Colville95@comcast.net>
2016-10-11 19:48:34 -0400

    Update sample_data_1.sql

--- sample_data_1.sql
+++ sample_data_1.sql
@@ -63,3 +63,37 @@ INSERT INTO Categories (categoryId, parentId, categoryName) VALUES (30, 5, "Sc-F
 
 /* Movies to sell */
 INSERT INTO Movies (title, year, synopsis) VALUES ("Aliens", 1979, "After a space merchant vessel perceives an unknown transmission as distress call, their landing on the source moon finds one of the crew attacked by a mysterious lifeform. Continuing their journey back to Earth with the attacked crew having recovered and the critter deceased, they soon realize that its life cycle has merely begun.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Shawshank Redemption", 1994, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Godfather", 1972, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Godfather: Part II", 1974, "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Dark Knight", 2008, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("12 Angry Men", 1957, "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Schindler's List", 1993, "In German-occupied Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazi Germans.")
+INSERT INTO Movies (title, year, synopsis) VALUES ("Pulp Fiction", 1994, "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Lord of the Rings: The Return of the King", 2003, "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Good, the Bad and the Ugly", 1966, "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Fight Club", 1999, "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Lord of the Rings: The Fellowship of the Ring", 2001, "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle Earth from the Dark Lord Sauron.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Star Wars: Episode V - The Empire Strikes Back", 1980, "After the rebels have been brutally overpowered by the Empire on their newly established base, Luke Skywalker takes advanced Jedi training with Master Yoda, while his friends are pursued by Darth Vader as part of his plan to capture Luke.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Forrest Gump", 1994, "Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Inception", 2010, "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Lord of the Rings: The Two Towers", 2002, "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("One Flew Over the Cuckoo's Nest", 1975, "A criminal pleads insanity after getting into trouble again and once in the mental institution rebels against the oppressive nurse and rallies up the scared patients.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Goodfellas", 1990, "Henry Hill and his friends work their way up through the mob hierarchy.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Matrix", 1990, "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Seven Samurai", 1954, "A poor village under attack by bandits recruits seven unemployed samurai to help them defend themselves.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Star Wars: Episode IV - A New Hope", 1977, "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("City of God", 2002, "Two boys growing up in a violent neighborhood of Rio de Janeiro take different paths: one becomes a photographer, the other a drug dealer.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("Se7en", 1995, "Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his modus operandi.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Silence of the Lambs", 1991, "A young F.B.I. cadet must confide in an incarcerated and manipulative killer to receive his help on catching another serial killer who skins his victims.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("It's a Wonderful Life", 1946, "An angel helps a compassionate but despairingly frustrated businessman by showing what life would have been like if he never existed.");
+INSERT INTO Movies (title, year, synopsis) VALUES ("The Usual Suspects", 1995, "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.");
+
+
+
+
+
+
+
+
+
